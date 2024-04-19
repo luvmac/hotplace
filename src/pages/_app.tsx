76bb48e6deb9238290e874ea7ed3f3d63d3b3cd0
luvmac <cotjdud1010@gmail.com>
2024-04-18 22:22:58 -0700

    useInfiniteQuery를 사용해서 데이터 순차적으로 불러오기

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,17 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
+import { QueryClient, QueryClientProvider } from "react-query";
+import { ReactQueryDevtools } from "react-query/devtools";
 
 export default function App({ Component, pageProps }: AppProps) {
-  return <Layout><Component {...pageProps} /></Layout>;
+  const queryClient = new QueryClient;
+  return (
+    <QueryClientProvider client={queryClient}> 
+      <Layout>
+        <Component {...pageProps} />
+      </Layout>
+    <ReactQueryDevtools />
+    </QueryClientProvider>
+  );
 }
