08aa3abe9f5942df67a6826ab20bd76703e61612
luvmac <cotjdud1010@gmail.com>
2024-04-19 14:49:13 -0700

    카카오 꾸글 네이버 프로바이더 추가 상세정보 데이터 가져오기 추가

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,11 +1,15 @@
 import React from 'react'
 import Link from "next/link"
+
+import { useSession, signOut } from "next-auth/react"
 import { useState } from "react";
 import { BiMenu } from 'react-icons/bi'
 import { AiOutlineClose } from 'react-icons/ai'
 
 export default function Navbar() {
-  const [isOpen, setIsOpen] = useState(false)
+  const [isOpen, setIsOpen] = useState(false);
+  const { data, status } = useSession();
+
   return <>
     <div className='navbar'>
       <Link href="/" className='navbar__logo'>nextMap</Link>
@@ -13,8 +17,12 @@ export default function Navbar() {
         <Link href="/stores" className='navbar__list--item'>맛집 목록</Link>
         <Link href="/stores/new" className='navbar__list--item'>맛집 등록</Link>
         <Link href="/users/likes" className='navbar__list--item'>찜한 가게</Link>
-        <Link href="/users/login" className='navbar__list--item'>로그인</Link>
+      {status === 'authenticated' ? <button type="button" onClick={() => signOut()}>
+        로그아웃
+      </button>
+      : <Link href="/api/auth/signin" className='navbar__list--item'>로그인</Link>}
       </div>
+      
       {/* mobild button */}
       <div 
           role="presentation" 
@@ -31,7 +39,7 @@ export default function Navbar() {
         <Link href="/stores" className='navbar__list--item--mobile'>맛집 목록</Link>
         <Link href="/stores/new" className='navbar__list--item--mobile'>맛집 등록</Link>
         <Link href="/users/likes" className='navbar__ist--item--mobile'>찜한 가게</Link>
-        <Link href="/users/login" className='navbar__list--item--mobile'>로그인</Link>
+        <Link href="/api/auth/signin" className='navbar__list--item--mobile'>로그인</Link>
       </div>
       </div>
     )}
--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -3,15 +3,21 @@ import type { AppProps } from "next/app";
 import Layout from "../components/Layout";
 import { QueryClient, QueryClientProvider } from "react-query";
 import { ReactQueryDevtools } from "react-query/devtools";
+import { SessionProvider } from "next-auth/react"
 
 export default function App({ Component, pageProps }: AppProps) {
   const queryClient = new QueryClient;
+  const { session } = pageProps;
+
   return (
     <QueryClientProvider client={queryClient}> 
+    <SessionProvider session={session}>
       <Layout>
         <Component {...pageProps} />
       </Layout>
     <ReactQueryDevtools />
+    </SessionProvider>
     </QueryClientProvider>
+    
   );
 }
