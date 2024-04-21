5b76b4ce6ed0758ffa1de0dad17a05839ba618c4
luvmac <cotjdud1010@gmail.com>
2024-04-21 12:28:02 -0700

    마이페이지 생성하고 데이터 패칭 방법 getStaticprops -> getServersideProps 로 바꾸어줌

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -25,11 +25,21 @@ export default function Home({ stores }: { stores: StoreType[]}) {
  
 
 }
-export async function getStaticProps() {
-  const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`)
+
+// 맛집 등록하면 1시간동안 업뎃이 안되는? 그런 오류
+// 사용자 경험이 너무 안좋게 때무네 getServerSideProps로 바꿔주기
+// export async function getStaticProps() {
+//   const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`)
  
+//   return {
+//     props: { stores: stores.data },
+//     revalidate: 60 * 60,
+//   }
+// }
+
+export async function getServerSideProps() {
+  const stores = await axios(`${process.env.NEXT_PUBLIC_API_URL}/api/stores`);
   return {
-    props: { stores: stores.data },
-    revalidate: 60 * 60,
+    props: { stores: stores.data }
   }
 }
