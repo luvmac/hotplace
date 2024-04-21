c0b0c3ed1c900d9935fa272b6d7dc6d53e56ffe8
luvmac <cotjdud1010@gmail.com>
2024-04-21 01:09:24 -0700

    삭제 기능 구현! delete method 작성 및 기능 구현

--- src/pages/api/stores.ts
+++ src/pages/api/stores.ts
@@ -15,6 +15,7 @@ export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<StoreApiResponse | StoreType[] | StoreType | null>
 ) {
+  // 딜리트 메소드 요청으 ㄹ위한 id 값 쿼리로 받기 위해서 id 추가 
   const { page = "", limit = "", q, district, id }: Responsetype = req.query;
   if (req.method === "POST") {
     // 데이터 생성을 처리한다
@@ -35,6 +36,8 @@ export default async function handler(
     });
 
     return res.status(200).json(result);
+
+
   } else if (req.method === "PUT") {
     // 데이터 수정을 처리한다
     const formData = req.body;
@@ -57,6 +60,7 @@ export default async function handler(
     return res.status(200).json(result);
   } else if (req.method === "DELETE") {
     // 데이터 삭제
+    // 아이디가 있을 경우에만 삭제 할 수 있도록 조건문 걸어주기 !
     if (id) {
       const result = await prisma.store.delete({
         where: {
--- src/pages/stores/[id]/index.tsx
+++ src/pages/stores/[id]/index.tsx
@@ -8,9 +8,11 @@ import { StoreType } from "@/interface";
 import Loader from "@/components/Loader";
 import Map from "@/components/Map";
 import Marker from "@/components/Marker";
+
 // 사용자가 로그인 햇을 때만 수정 폼에 접근 할 수 있도록 분기처리
 import { useSession } from "next-auth/react"
 import Link from "next/link";
+import { toast } from "react-toastify";
 
 
 export default function StorePage() {
@@ -34,6 +36,28 @@ export default function StorePage() {
     enabled: !!id,
     refetchOnWindowFocus: false,
   });
+  
+  const handleDelete = async() => {
+    const confirm = window.confirm("해당 가게를 삭제하시겠습니까?");
+    // store?. < 이부분 언디파인드 일 수 있으니까 confirm && store 이 부분을 추가한다! 
+    // 다른 로직들도 이런식으로 해주면 타입 에러를 해결할 수 있찌 않을까?
+    if(confirm && store) {
+      try {
+        const result = await axios.delete(`/api/stores?id=${store?.id}`);
+
+        if(result.status === 200) {
+          toast.success("가게를 삭제했습니다. !");
+          router.replace("/")
+        } else {
+          toast.error("다시 시도해주세요.")
+        }
+      } catch (error) {
+        console.log(error);
+        toast.error("다시 시도해주세요.")
+      }
+    }
+  };
+
 
   if (isError) {
     return (
@@ -59,8 +83,8 @@ export default function StorePage() {
             {store?.address}
           </p>
         </div>
-
-        <div className="flex items-center gap-4">
+        {status === 'authenticated' && (
+          <div className="flex items-center gap-4 px-4 py-3">
           <Link 
           className="underline hover:text-gray-400 text-sm" 
           href={`/stores/${store?.id}/edit`}
@@ -69,10 +93,13 @@ export default function StorePage() {
           </Link>
           <button 
           type="button" 
-          className="underline hover:text-gray-400 text-sm">
+          className="underline hover:text-gray-400 text-sm"
+          onClick={handleDelete}>
             삭제
           </button>
         </div>
+        )}
+        
 
 
         </div>
